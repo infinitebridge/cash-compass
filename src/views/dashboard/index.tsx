9e0b004d468b4afba841984afbd353e15c09afc0
Othmane Kahtal <contact@othmanekahtal.me>
2025-03-27 22:23:24 +0000

    fix: update order of sections in dashboard

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -58,8 +58,8 @@ function Dashboard() {
       />
       <SectionCards />
       <ExpensesRevenueTrend />
-      <RecentTransactions />
       <ExpensesRevenueDonuts />
+      <RecentTransactions />
       <CashFlowProjection />
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
